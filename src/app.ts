30d5c631b972fcaec056dd9b8a9b706d45a80244
CarlosSuarezJS20 <69536882+CarlosSuarezJS20@users.noreply.github.com>
2021-04-13 10:14:04 +0100

    started project. Display elements finished

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,56 @@
-// Code goes here!
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+  titleInputEl: HTMLInputElement;
+  descriptionTextAreaEl: HTMLTextAreaElement;
+  peopleInputEl: HTMLInputElement;
+
+  constructor() {
+    this.templateElement = document.getElementById(
+      "project-input"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = "user-input";
+    this.titleInputEl = this.element.querySelector(
+      "#title"
+    )! as HTMLInputElement;
+    this.descriptionTextAreaEl = this.element.querySelector(
+      "#description"
+    )! as HTMLTextAreaElement;
+    this.peopleInputEl = this.element.querySelector(
+      "#people"
+    )! as HTMLInputElement;
+
+    this.configure();
+    this.attach();
+  }
+
+  private submitHandler(event: Event) {
+    console.log(
+      this.titleInputEl,
+      this.descriptionTextAreaEl,
+      this.peopleInputEl
+    );
+    event.preventDefault();
+    const titleElValue: string = this.titleInputEl.value;
+    const descriptionTextAreaElValue: string = this.descriptionTextAreaEl.value;
+    const peopleInputValue: number = +this.peopleInputEl.value;
+  }
+
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler.bind(this));
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
+  }
+}
+
+const projectInstant = new ProjectInput();
